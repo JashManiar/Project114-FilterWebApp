495113a41bddff22b28576d33e0cb2be863fbd63
Jash Maniar <101001262+JashManiar@users.noreply.github.com>
2022-09-17 18:36:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(300,300);
+    canvas = createCanvas(250,250);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(200,200);
