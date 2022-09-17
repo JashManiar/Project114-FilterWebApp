0bc49c2f7468bb415e19762079f112d783545b69
Jash Maniar <101001262+JashManiar@users.noreply.github.com>
2022-09-17 18:40:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 mustache_x=0;
 mustache_y=0;
 
-
 function preload(){
     mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup(){
-    canvas = createCanvas(250,250);
+    canvas = createCanvas(300,300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(200,200);
+    video.size(300,300);
     video.hide();
 
     poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPose);
-    
-
 }
 
 function draw(){
