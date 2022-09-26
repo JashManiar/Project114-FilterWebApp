ceaacf041e65a9a3ec9bea082cdc130134a7d059
Jash Maniar <101001262+JashManiar@users.noreply.github.com>
2022-09-26 19:33:55 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-mustache_x=0;
-mustache_y=0;
+hairY=0;
+hairX=0;
+noseX=0;
+noseY=0;
+moustache_x=0;
+moustache_y=0;
+filters ="";
 
 function preload(){
-    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+    moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+    lipstick = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
+    hair = loadImage("https://i.postimg.cc/gJNcPKsy/My-project-1.png");
 }
 
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(200,200);
+    video.size(300,300);
     video.hide();
+    canvas.position(550,250);
 
     poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPose);
@@ -18,7 +26,19 @@ function setup(){
 
 function draw(){
     image(video,0,0,300,300);
-    image(mustache,mustache_x,mustache_y,60,45);
+    if(filters == "moustache")
+    {
+        image(moustache,moustache_x,moustache_y,60,45);
+    }
+    if(filters == "lipstick")
+    {
+        image(lipstick,noseX,noseY,60,45);
+    }
+    if(filters == "hair")
+    {
+        image(hair,hairX,hairY,200,105);
+    }
+    
 }
 
 function take_snapshot(){
@@ -32,9 +52,26 @@ function modelLoaded(){
 function gotPose(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x: "+results[0].pose.nose.x);
-        mustache_x = results[0].pose.nose.x-30;
-        console.log("nose y: "+results[0].pose.nose.y);
-        mustache_y = results[0].pose.nose.y;
+        noseX =  results[0].pose.nose.x-30;
+        moustache_x = results[0].pose.nose.x-30;
+        noseY = results[0].pose.nose.y+30;
+        moustache_y = results[0].pose.nose.y;
+        hairX = results[0].pose.nose.x-90;
+        hairY = results[0].pose.nose.y-155;
     };
+}
+
+function moustache_function()
+{
+    filters = "moustache";
+}
+
+function lipstick_function()
+{
+    filters = "lipstick";
+}
+
+function hair_function()
+{
+    filters = "hair";
 }
